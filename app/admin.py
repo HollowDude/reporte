0899004b632c766f9da5e8e8c7dc18f3b9a1c6a1
HollowDude <madafakn@gmail.com>
2025-03-06 23:25:14 -0500

    Modelo garantia con vista admin y correccion del correo

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Reporte
+from .models import Reporte, Garantia
 
 @admin.register(Reporte)
 class ReporteAdmin(admin.ModelAdmin):
@@ -7,5 +7,26 @@ class ReporteAdmin(admin.ModelAdmin):
     search_fields = ('vin', 'nombre', 'apellidos', 'email', 'telefono')
     list_filter = ('fecha_armado', 'fecha_entregado')
     ordering = ('-numero_reporte',)
+
+@admin.register(Garantia)
+class GarantiaAdmin(admin.ModelAdmin):
+    list_display = ('get_usuario_nombre_completo', 'get_usuario_dir', 'get_usuario_tell', 'vin', 'modelo', 'motor_num', 'fecha_fabr', 'peso', 'voltaje', 'potencia', 'motivo', 'evaluacion', 'trabajos_hechos', 'piezas_usadas', 'recomendaciones', 'nombre_especialista', 'conformidad_cliente', 'facturar_a')
+    search_fields = ('get_usuario_nombre_completo', 'get_usuario_tell', 'evaluacion', 'nombre_especialista', 'facturar_a')
+    ordering = ('-fecha_fabr',)
+
+    @admin.display(empty_value="???")
+    def get_usuario_nombre_completo(self, obj):
+        return f"{obj.usuario.nombre} {obj.usuario.apellidos}"
+    @admin.display(empty_value="???")
+    def get_usuario_dir(self, obj):
+        return f"{obj.usuario.direccion}"
+    @admin.display(empty_value="???")
+    def get_usuario_tell(self, obj):
+        return f"{obj.usuario.telefono}"
+    
+    get_usuario_dir.short_description = "Usuario direccion"
+    get_usuario_tell.short_description = "Usuario telefono"
+    get_usuario_nombre_completo.short_description = 'Nombre del usuario' 
+
 
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Reporte(models.Model):
     apellidos = models.CharField(max_length=100)
     carnet = models.CharField(max_length=50)
     direccion = models.TextField()
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     telefono = models.CharField(max_length=20)
     fecha_armado = models.DateField()
     fecha_entregado = models.DateField()
@@ -25,3 +25,21 @@ def set_numero_reporte(sender, instance, **kwargs):
     if not instance.numero_reporte:
         last_reporte = Reporte.objects.order_by('-numero_reporte').first()
         instance.numero_reporte = (last_reporte.numero_reporte + 1) if last_reporte else 1
+
+class Garantia(models.Model):
+    usuario = models.ForeignKey(Reporte, on_delete=models.CASCADE)
+    vin=models.CharField(max_length=255, primary_key=True)
+    modelo = models.CharField(max_length=50)
+    motor_num=models.CharField(max_length=255)
+    fecha_fabr= models.DateField()
+    peso=models.FloatField()
+    voltaje=models.IntegerField()
+    potencia=models.FloatField()
+    motivo=models.TextField()
+    evaluacion=models.TextField()
+    trabajos_hechos=models.TextField()
+    piezas_usadas=models.TextField()
+    recomendaciones=models.TextField()
+    nombre_especialista=models.CharField(max_length=50)
+    conformidad_cliente=models.BooleanField()
+    facturar_a=models.CharField(default="Cliente")
