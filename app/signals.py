86f2f7e4d53703c42d38b704b4c41d6a647e7a56
Systemregister24 <systemregister24@gmail.com>
2025-03-14 13:12:51 -0400

    signals.py

--- app/signals.py
+++ app/signals.py
@@ -9,7 +9,7 @@ def enviar_notificacion_email(sender, instance, **kwargs):
     if instance.llamada:
         subject = "Notificación de Cliente Contactado"
         message = f"El cliente {instance.cliente} ha sido contactado."
-        recipient_list = ["brayan170504@gmail.com"]  # Cambia esto al correo de destino
+        recipient_list = ["tallerjireh47@gmail.com ; ailetmariacasal0@gmail.com"]  # Cambia esto al correo de destino
 
         send_mail(subject, message, None, recipient_list)
         print(f"Correo enviado a {recipient_list}")
