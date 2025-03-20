b1305c004c5cec082ab06179e9622e0396379d5e
HollowDude <madafakn@gmail.com>
2025-03-20 04:03:57 -0500

    Solving requirements and adding updates on model registro about the fecha_entrega and TiempoRestante

--- reporte/settings.py
+++ reporte/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,6 +92,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'reporte.wsgi.application'
+
+CELERY_BEAT_SCHEDULE = {
+    'actualizar_tiempoR_diario': {
+        'task': 'app.tasks.actualizar_tiempoR',
+        'schedule': crontab(hour=0, minute=0),
+    },
+}
 
 
 # Database
