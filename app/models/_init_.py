4a2aa8d8f29cd85cd5ab4ec3bbb08ab47bfcaf67
HollowDude <madafakn@gmail.com>
2025-04-02 20:53:18 -0500

    Big update, panels and more

--- app/models/_init_.py
+++ app/models/_init_.py
@@ -3,3 +3,4 @@ from app.models.triciclo import Triciclo
 from app.models.registro import Registro_Pendiente
 from app.models.cliente import Cliente
 from app.models.empresa import Empresa
+from app.models.panels import Panels
